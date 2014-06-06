253d3c3592fffb754c1901e69f8cbb9045e546d5
Matt Ahlschwede <perscrumption@hotmail.com>
2014-06-06 02:31:56 -0500

    Update player.py

--- player.py
+++ player.py
@@ -4,36 +4,90 @@ import ss
 class Player(pygame.sprite.Sprite):
 	def __init__(self, level, *groups):
 		super(Player, self).__init__(*groups)
+		self.level = level
 		self.Rimg = pygame.image.load('RangerDanR.png')
 		self.Limg = pygame.image.load('RangerDanL.png')
 		self.image = self.Rimg
-		self.rect = pygame.rect.Rect((100,100), self.image.get_size())#320,240
-		self.level = level
+		self.spawnx = 1
+		self.spawny = 1
+		self.indx = self.spawnx
+		self.indy = self.spawny
+		self.rect = pygame.rect.Rect((self.spawnx * self.level.tilex, self.spawny * self.level.tiley), self.image.get_size())#320,240
+	
+	def spawn(self):
+		self.indx = self.spawnx
+		self.indy = self.spawny
+		self.rect = pygame.rect.Rect((self.spawnx * self.level.tilex, self.spawny * self.level.tiley), self.image.get_size())	
+		
+	def set_spawn(self, x, y):
+		self.spawnx = x
+		self.spawny = y
 		
 	def command(self, cmd):
 		prev = self.rect.copy()
+		#move up
 		if cmd == "U":
+			#self.indy -= 1
 			if self.rect.y <= 100:
+				#if self.level.maptiles[
 				self.level.move_BG("D")
 			else:
 				self.rect.y -= 100
+		#move down
 		if cmd == "D":
+			#self.indy += 1
 			if self.rect.y >= self.level.mastery - 200:
 				self.level.move_BG("U")
 			else:
 				self.rect.y += 100
+		# move left
 		if cmd == "L":
 			self.image = self.Limg
 			if self.rect.x <= 100:
 				self.level.move_BG("R")
 			else:
 				self.rect.x -= 100
+		#move up and left 
+		if cmd == "UL":
+			self.image = self.Limg
+			if self.rect.x <= self.level.tilex or self.rect.y <= self.level.tiley:
+				self.level.move_BG("LR")
+			else:
+				self.rect.x -= 100
+				self.rect.y -= 100		
+		#move down and left
+		if cmd == "LL":
+			self.image = self.Limg
+			if self.rect.x <= self.level.tilex or self.rect.y >= self.level.mastery - 2*self.level.tiley:
+				self.level.move_BG("UR")
+			else:
+				self.rect.x -= 100
+				self.rect.y += 100
+		#move right
 		if cmd == "R":
 			self.image = self.Rimg
 			if self.rect.x >= self.level.masterx - 200:
 				self.level.move_BG("L")
 			else:
 				self.rect.x += 100
+		#move up and right
+		if cmd == "UR":
+			self.image = self.Rimg
+			if self.rect.x >= self.level.masterx - 200 or self.rect.y <= self.level.tiley:
+				self.level.move_BG("LL")
+			else:
+				self.rect.x += 100
+				self.rect.y -= 100
+		#move down and right
+		if cmd == "LR":
+			self.image = self.Rimg
+			if self.rect.x >= self.level.masterx - 200 or self.rect.y >= self.level.mastery - 2*self.level.tiley:
+				self.level.move_BG("UL")
+			else:
+				self.rect.x += 100
+				self.rect.y += 100
+				
+				
 		new = self.rect
 		for loc in pygame.sprite.spritecollide(self, self.level.unpassable, False):
 			loc = loc.rect
