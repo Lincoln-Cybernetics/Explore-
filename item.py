806a296eecafde58315aeb77c9f1be9c60ccf257
Matt Ahlschwede <perscrumption@hotmail.com>
2014-06-10 22:43:46 -0500

    Update item.py
    adds the AXE

--- item.py
+++ item.py
@@ -3,21 +3,42 @@ import ss
 
 
 class Item(pygame.sprite.Sprite):
-	def __init__(self, level, *groups):
+	def __init__(self, level, flav, *groups):
 		super(Item, self).__init__(*groups)
 		#self.image = pygame.image.load('gem.png')
 		self.sheet = pygame.image.load('exp100.png').convert()
 		self.animator = ss.Cutout(self.sheet, 100, 100)
-		self.animator.set_Img(6,0)
-		self.image = self.animator.get_Img().convert()
-		self.image.set_colorkey((255,0,0))
+		#self.animator.set_Img(6,0)
+		#self.image = self.animator.get_Img().convert()
+		#self.image.set_colorkey((255,0,0))
 		self.level = level
-		self.rect = pygame.rect.Rect((100,100), self.image.get_size())
-		self.flavor = 'gem'
+		
+		#self.flavor = 'default'
+		self.flavor_saver = ['gem', 'axe']
 		self.x = 0
 		self.y = 0
+		self.set_type(flav)
+		#0 = gem
+		#1 = axe
 		
-
+	def set_type(self, item):
+		self.flavor = self.flavor_saver[item]
+		if item == 0:
+			xind = 6
+			yind = 0
+			self.flavor = 'gem'
+		elif item == 1:
+			xind = 6
+			yind = 5
+			self.flavor = 'axe'
+		else:
+			xind = 6
+			yind = 0
+			self.flavor = 'default'
+		self.animator.set_Img(xind, yind)
+		self.image = self.animator.get_Img().convert()
+		self.image.set_colorkey((255,0,0))
+		self.rect = pygame.rect.Rect((100,100), self.image.get_size())
 
 	def set_Index(self, x, y):
 		self.x = x
