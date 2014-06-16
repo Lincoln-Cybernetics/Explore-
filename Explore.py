c59df4850248f95af266626143765427869eecd0
Matt Ahlschwede <perscrumption@hotmail.com>
2014-06-16 02:29:25 -0500

    Update Explore.py
    fresh slate

--- Explore.py
+++ Explore.py
@@ -1,16 +1,163 @@
 import pygame
-import random
 import player
-import landform
+import ss
+import tile
+import random
 import item
-import time
-import Mob
+import mob
 
 class Game(object):
-	
-	#Moves the background
-    def move_BG(self, d):
+	def main (self, screen):
+		
+		#general signals
+		self.Game_Over = 0
+		self.Turn_Over = 0
+		self.winx = mainx
+		self.winy = mainy
+		
+		#spritesheet dis-aggregator
+		self.tilex = 100
+		self.tiley = 100
+		self.sheet = pygame.image.load('exp100.png').convert()
+		self.animator = ss.Cutout(self.sheet, self.tilex, self.tiley)
+		
+		#sprite groups
+		self.players = pygame.sprite.Group()
+		self.mobs = pygame.sprite.Group()
+		self.mymobs = []
+		self.terrain = pygame.sprite.Group()
+		self.mymap = []
+		self.space = pygame.sprite.Group()
+		self.items = pygame.sprite.Group()
+		self.background = pygame.sprite.Group()
+		
+		#player
+		self.player1 = player.Player(self, self.players)
+		
+		
+		self.mapgen(19,19,'Random')
+		self.iterate_Game()
+		
+	def mapgen(self, x,y, maptype):
+		mygem = item.Item(self, self.items)
+		mygem.set_type(0)
+		myaxe = item.Item(self, self.items)
+		myaxe.set_type(1)
+		mydude = mob.Mob(self, self.mobs)
+		mydude.set_type(0)
+		self.mymobs.append(mydude)
+		for a in range(x):
+			maprow = []
+			for b in range(y):
+				if maptype == 'Basic':
+					landtype = 1
+				if maptype == 'Random':
+					landtype = random.randrange(15)+1
+				acre = tile.Land(self, self.terrain)
+				if a == 0 or b == 0 or a == x-1 or b == y-1:
+					acre.set_type(0)
+					self.space.add(acre)
+				else:
+					acre.set_type(landtype)
+				acre.spawn(a, b)
+				self.background.add(acre)
+				maprow.append(acre)
+			self.mymap.append( maprow )
+		self.player1.spawn(1,1)
+		if maptype == 'Basic':
+			mygem.spawn(4,5)
+			myaxe.spawn(5,4)
+			mydude.spawn(6,6)
+		if maptype == 'Random':
+			mygem.spawn(random.randrange(x-2)+1, random.randrange(y-2)+1)
+			myaxe.spawn(random.randrange(x-2)+1, random.randrange(y-2)+1)
+			mydude.spawn(random.randrange(x-2)+1, random.randrange(y-2)+1)
+		self.background.add(mygem)
+		self.background.add(myaxe)
+		self.background.add(mydude)
+		
+	def iterate_Game(self):
+		while self.Game_Over == 0:
+			
+			self.Turn_Over = 0
+			self.display()
+			self.player1.AP_c = self.player1.AP_max
+			for dude in self.mobs:
+				dude.AP_c = dude.AP_max
+			
+			while self.Turn_Over == 0:
+				if self.Game_Over != 0:
+							break		
+				for event in pygame.event.get():
+						if event.type == pygame.QUIT:
+							return
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+							return
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+							self.Turn_Over = 1
+							break
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP8:
+							self.player1.command("U")
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP2:
+							self.player1.command("D")
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP4:
+							self.player1.command("L")
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP6:
+							self.player1.command("R")	
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP7:
+							self.player1.command("UL")	
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP9:
+							self.player1.command("UR")	
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP1:
+							self.player1.command("LL")	
+						if event.type == pygame.KEYDOWN and event.key == pygame.K_KP3:
+							self.player1.command("LR")	
+							
+						self.display()		
+			for dude in self.mobs:
+				dude.take_turn()			
+			while self.Turn_Over == 1:
+				if self.Game_Over:
+					break
+				# Display some text
+				font = pygame.font.Font(None, 64)
+				text = font.render("Turn Over", 1, (255, 10, 10), (0,0,255))
+				# Blit everything to the screen
+				screen.blit(text, (self.winx/2-self.tilex, self.winy/2-self.tiley))
+				pygame.display.flip()
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						return
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+						return
+					if event.type == pygame.KEYDOWN and  (event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER):
+						self.Turn_Over = 0
+						break
+						
+						
+		if self.Game_Over == 1:
+			print "You have walked off the edge of the world."
+			print "You are doomed to drift away into OUTER SPACE."
+			return
+		
+		if self.Game_Over == 2:
+			print "You got the gem."
+			print "You win!"
+			return
+		
+		
+		
+	def display(self):
+		self.terrain.draw(screen)
+		self.mobs.draw(screen)
+		self.items.draw(screen)
+		self.players.draw(screen)
+		
+		pygame.display.flip()
+		
+	def move_BG(self, d):
 		for tile in self.background:
+			#tile.set_type(tile.fnum)
 			if d == 'U':
 				tile.set_Index(tile.get_Index('X'), tile.get_Index('Y')-1)
 			if d == 'D':
@@ -27,199 +174,8 @@ class Game(object):
 				tile.set_Index(tile.get_Index('X')-1, tile.get_Index('Y')+1)
 			if d == 'LR':
 				tile.set_Index(tile.get_Index('X')+1, tile.get_Index('Y')+1)
-				
-    #map generation
-    def map_gen(self, kind):
-		
-		if kind == 'Random':
-			self.player1.set_spawn(random.randrange(self.masterx//self.tilex), random.randrange(self.mastery//self.tiley))
-				
-		
-		mygem = item.Item(self, 0, self.items)
-		#mygem.set_type(0)
-		myaxe = item.Item(self, 1, self.items)
-		#myaxe.set_type(1)
-		dude = Mob.Mob(self, self.baddies)
-		
-		for a in range(self.masterx//self.tilex):
-			maprow = []
-			for b in range(self.mastery//self.tiley):
-				#basic map
-				if kind == 'Basic':
-					tertype = 0
-					mygem.set_Index(5,5)
-					myaxe.set_Index(8,2)
-					dude.set_spawn(2,6)
-					dude.spawn()
-					dude.set_type("Charger")
-					
-				#Random map
-				if kind == 'Random':
-					tertype = random.randrange(15)
-					mygem.set_Index(random.randrange(self.masterx//self.tilex), random.randrange(self.mastery//self.tiley))
-					myaxe.set_Index(random.randrange(self.masterx//self.tilex), random.randrange(self.mastery//self.tiley))
-					dude.set_spawn(random.randrange(self.masterx//self.tilex), random.randrange(self.mastery//self.tiley))
-					dude.spawn()
-					
-				if a == self.player1.spawnx and b == self.player1.spawny:
-					tertype = 	0
-				plot = landform.Acre(self, self.terrain)
-				plot.set_Index(a,b)
-				plot. set_Biome(tertype)
-				maprow.append(plot)
-				self.background.add(plot)
-				if plot.flavor == 'Whirlpool' or plot.flavor == 'Ocean' or plot.flavor == 'Active Volcano':
-					self.player1.unpassable.add(plot)
-			self.maptiles.append(maprow)
-		self.background.add(self.items)
-		self.background.add(self.baddies)
-		self.mobs.add(self.baddies)
-		self.mobs.add(self.player1)
-		self.player1.unpassable.add(self.baddies)
-		for badguy in self.baddies:
-			badguy.unpassable.add(self.player1)
-				
-	#MAIN
-    def main(self, screen):
-		
-		
-		#is the Game Over?
-		self.Game_Over = False
-		self.end_turn = False
-		self.to_flag = False
-		#why is the game over?
-		self.GOstr = ""
-		
-		#sprite groups
-		self.sprites = pygame.sprite.Group()
-		self.items = pygame.sprite.Group()
-		self.terrain = pygame.sprite.Group()
-		self.background = pygame.sprite.Group()
-		self.baddies = pygame.sprite.Group()
-		self.mobs = pygame.sprite.Group()
-		
-		
-		#list of map tiles
-		self. maptiles = []
-		
-		#masterx & mastery are the dimensions of the game window
-		self.masterx = mainx
-		self.mastery = mainy
-		
-		#
-		
-		#tilex & tiley are the dimensions of the graphics tiles
-		self.tilex = 100
-		self.tiley = 100
-		
-		#the player
-		self.player1 = player.Player(self, self.sprites)
-		
-		#make a map
-		
-		self.map_gen('Basic')
-		
-		#spawn the player
-		self.player1.spawn()
-		
-		#main loop
-		while self.Game_Over == False:
-			self.end_turn = False
-			self.to_flag = False
-			for mob in self.mobs:
-				mob.AP_c = mob.AP_max
-			while self.end_turn == False:
-				
-				for event in pygame.event.get():
-					if event.type == pygame.QUIT:
-						return
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-						return
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-						self.end_turn = True
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP8:
-						self.player1.command("U")
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP2:
-						self.player1.command("D")
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP4:
-						self.player1.command("L")
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP6:
-						self.player1.command("R")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP7:
-						self.player1.command("UL")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP9:
-						self.player1.command("UR")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP1:
-						self.player1.command("LL")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_KP3:
-						self.player1.command("LR")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
-						self.player1.command("CHOP")	
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-						self.player1.command("Plant")
-						
-				for badguy in self.baddies:
-					if badguy.Alive == False:
-						badguy.kill()		
-				self.iterate_Game()
-				if self.Game_Over:
-					break
-					
-					
-			while self.to_flag	== False:
-				if self.Game_Over:
-					break
-				# Display some text
-				font = pygame.font.Font(None, 64)
-				text = font.render("Turn Over", 1, (255, 10, 10), (0,0,255))
-				# Blit everything to the screen
-				screen.blit(text, (mainx/2-self.tilex, mainy/2-self.tiley))
-				pygame.display.flip()
-				for event in pygame.event.get():
-					if event.type == pygame.QUIT:
-						return
-					if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-						return
-					if event.type == pygame.KEYDOWN and (event.key == pygame.K_RETURN or event.key == pygame.K_KP_ENTER):
-						self.to_flag = True
-			
-			if self.Game_Over:
-				pass
-			else:
-				for badguy in self.baddies:
-					badguy.take_turn()
-					if badguy.Alive == False:
-						badguy.kill()			
-				self.iterate_Game()
-			
-		if self.GOstr == "SPACE":
-			print "You have walked off the edge of the world.\nYou are now doomed to spend eternity drifting in OUTER SPACE!"
-		if self.GOstr == "WIN":
-			time.sleep(2)
-			print" You Win!  YAY!"
-		if self.GOstr == "Dead":
-			print"Tried and Died."
-		return
-			
-    def iterate_Game(self):
-		
-		self.terrain.update()
-		self.sprites.update()
-		
-		screen.fill((0,0,0))
-		
-		self.terrain.draw(screen)
-		self.items.draw(screen)
-		self.baddies.draw(screen)
-		self.sprites.draw(screen)
-	
-		pygame.display.flip()
-        
 
-
-
-
-########################################################################					
+########################################################################
 if __name__ == '__main__':
 	pygame.init()
 	mainx = 1200
