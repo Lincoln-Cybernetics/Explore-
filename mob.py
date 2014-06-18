84761c1f21e2dcc9b7e66c506aa2ef0819613b51
Matt Ahlschwede <perscrumption@hotmail.com>
2014-06-18 03:13:41 -0500

    Update mob.py
    added: "Wanderer" and "Charger" personalities

--- mob.py
+++ mob.py
@@ -1,10 +1,14 @@
 import pygame
+import random
 
 class Mob(pygame.sprite.Sprite):
 	def __init__(self, level, *groups):
 		super(Mob, self).__init__(*groups)
 		#the game level
 		self.level = level
+		
+		#sprite groups
+		self.unpassable = pygame.sprite.Group()
 		
 		#base image
 		self.level.animator.set_Img(4,5)
@@ -14,6 +18,7 @@ class Mob(pygame.sprite.Sprite):
 		#type
 		self.flavor = "Static"
 		self.flavor_saver = ["Static", "Lemming", "Wanderer", "Charger"]
+		self.ATflag = 0#All-Terrain flag
 		
 		#location
 		self.firstflag = True
@@ -34,19 +39,49 @@ class Mob(pygame.sprite.Sprite):
 		self.HP_c = 5
 		self.ATT = 2
 		self.DEF = 1
-		self.DMG = 1
+		self.DMG = 0
 		
 	def set_type(self, personality):
 			self.flavor = self.flavor_saver[personality]
+			#static
 			if personality == 0:
 				xind = 4
 				yind = 5
+				self.ATflag = 0
+			#lemming
+			if personality == 1:
+				xind = 4
+				yind = 5
+				self.ATflag = 0
+			#wanderer
+			if personality == 2:
+				xind = 4
+				yind = 5
+				self.ATflag = 1
+			#charger
+			if personality == 3:
+				xind = 4
+				yind = 5
+				self.ATflag = 0
+			#default
 			else:
 				xind = 4
 				yind = 5
+				self.ATflag = 0
+				
+				
 			self.level.animator.set_Img(xind,yind)
 			self.image = self.level.animator.get_Img().convert()
 			self.image.set_colorkey((255,0,0))
+	
+	def fight(self, opponent):
+		if self.ATT > opponent.DEF:
+			opponent.damage(self.DMG)				
+	
+	def damage(self, dmg):
+		self.HP_c -= dmg
+		if self.HP_c <= 0:
+			self.Alive = False						
 		
 	def spawn(self,x,y):
 		self.scrnx = x
@@ -59,34 +94,87 @@ class Mob(pygame.sprite.Sprite):
 		for space in pygame.sprite.spritecollide(self, self.level.space, False):
 			self.Alive = False
 			self.kill()
+			
+	def mobcheck(self):
+		for mob in pygame.sprite.spritecollide(self, self.level.fightable, False):
+			if mob == self:
+				pass
+			else:
+				self.fight(mob)
+				if mob.Alive == False:
+					mob.kill()
+					return True
+				return False
+		return True
 		
 	def command(self, cmd):
-		if self.reckonAP(self.APcost[cmd]):
-			if cmd == "U":
-				self.move("U")
-			if cmd == "D":
-				self.move("D")
-			if cmd == "L":
-				self.move("L")
-			if cmd == "R":
-				self.move("R")
-			if cmd == "UL":
-				self.move("UL")
-			if cmd == "UR":
-				self.move("UR")
-			if cmd == "LL":
-				self.move("LL")
-			if cmd == "LR":
-				self.move("LR")
-					
-		else:
-			pass	
+		#reference of old location data
+		prevrect = self.rect.copy()
+		pxs = self.scrnx
+		pys = self.scrny
+		pmx = self.mapx
+		pmy = self.mapy
+		
+		if cmd == "U":
+			if self.level.mymap[self.mapx][self.mapy-1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("U")
+		if cmd == "D":
+			if self.level.mymap[self.mapx][self.mapy+1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("D")
+		if cmd == "L":
+			if self.level.mymap[self.mapx-1][self.mapy] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("L")
+		if cmd == "R":
+			if self.level.mymap[self.mapx+1][self.mapy] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("R")
+		if cmd == "UL":
+			if self.level.mymap[self.mapx-1][self.mapy-1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("UL")
+		if cmd == "UR":
+			if self.level.mymap[self.mapx+1][self.mapy-1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("UR")
+		if cmd == "LL":
+			if self.level.mymap[self.mapx-1][self.mapy+1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("LL")
+		if cmd == "LR":
+			if self.level.mymap[self.mapx+1][self.mapy+1] in self.unpassable:
+				pass
+			else:
+				if self.reckonAP(self.APcost[cmd]):
+					self.move("LR")
 			
+		self.spacecheck()
+		if self.mobcheck() == False:
+			self.rect = prevrect
+			self.scrnx = pxs
+			self.scrny = pys
+			self.mapx = pmx
+			self.mapy = pmy	
+									
 		if self.AP_c <= 0:
 			self.Turn_Over = True
 			
-		self.spacecheck()
-		
 				
 	def move(self, vec):
 		if vec == "U":
@@ -152,6 +240,7 @@ class Mob(pygame.sprite.Sprite):
 
 	def take_turn(self):
 		self.Turn_Over = False
+		
 		if self.flavor == "Static":
 			self.Turn_Over = True
 			return
@@ -159,16 +248,33 @@ class Mob(pygame.sprite.Sprite):
 		if self.flavor == "Lemming":
 			while self.Turn_Over == False:
 				self.command("L")
-				if self.AP_c <= 0:
-					self.Turn_Over = True
+			return
+						
+		if self.flavor == "Wanderer":
+			while self.Turn_Over == False:
+				dirs = ["U", "D", "L", "R", "UL", "UR", "LL", "LR"]
+				self.command(random.choice(dirs))
+			return
 			
-	def fight(self, opponent):
-		if self.ATT > opponent.DEF:
-			opponent.damage(self.DMG)
+		if self.flavor == "Charger":
+			while self.Turn_Over == False:
+				if self.AP_c >= 2:
+					if self.mapx > self.level.player1.mapx and self.mapy > self.level.player1.mapy:
+						self.command("UL")
+					if self.mapx < self.level.player1.mapx and self.mapy > self.level.player1.mapy:
+						self.command("UR")
+					if self.mapx > self.level.player1.mapx and self.mapy < self.level.player1.mapy:
+						self.command("LL")
+					if self.mapx < self.level.player1.mapx and self.mapy < self.level.player1.mapy:
+						self.command("LR")
+				if self.mapx > self.level.player1.mapx:
+					self.command("L")
+				if self.mapx < self.level.player1.mapx:
+					self.command("R")
+				if self.mapy > self.level.player1.mapy:
+					self.command("U")
+				if self.mapy < self.level.player1.mapy:
+					self.command("D")
+			return
 			
-	def damage(self, dmg):
-		self.HP_c -= dmg
-		if self.HP_c <= 0:
-			self.Alive = False
-			
-			
+		return
