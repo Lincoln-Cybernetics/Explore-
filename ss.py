c27c823ea0157ea5d678fd0e376d2fe349269411
Matt Ahlschwede <perscrumption@hotmail.com>
2014-06-29 15:53:19 -0500

    Update ss.py
    adds colorkey support

--- ss.py
+++ ss.py
@@ -12,6 +12,8 @@ class Cutout(object):
 		self.srcimg = src
 		self.img = pygame.Surface(self.rsource.size).convert()
 		self.img.blit(self.srcimg, (0,0), self.rsource)
+		self.cktog = False
+		self.ck = (0,255,0)
 		
 	def set_Sheet(self, src, w, h):
 		#change the sprite sheet
@@ -26,7 +28,18 @@ class Cutout(object):
 		self.rsource = pygame.Rect(myx,myy,self.w,self.h)
 		self.img = pygame.Surface(self.rsource.size).convert()
 		self.img.blit(self.srcimg, (0,0), self.rsource)
+		self.cktog = False
 		
 	def get_Img(self):
+		#applies colorkey
+		if self.cktog:
+			self.img.set_colorkey(self.ck)
 		#returns the set image
 		return self.img
+		
+	def set_colorkey(self, tog= True, col= (0,255,0)):
+		#toggles colorkey on or off
+		self.cktog = tog
+		#sets colorkey color
+		self.ck = col
+		
