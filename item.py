c357a418842b8e9b87f750726e5dc30b8313f8b6
Matt Ahlschwede <perscrumption@hotmail.com>
2014-07-22 04:21:02 -0500

    Update item.py
    adds the menu sprite

--- item.py
+++ item.py
@@ -6,9 +6,9 @@ class Item(pygame.sprite.Sprite):
         #the game level
         self.level = level
         #base image
-        self.level.animator.set_Img(6,0)
-        self.level.animator.set_colorkey((255,0,0))
-        self.image = self.level.animator.get_Img().convert()
+        self.level.warehouse.set_Img(6,0)
+        self.level.warehouse.set_colorkey(True,(255,0,0))
+        self.image = self.level.warehouse.get_Img().convert()
         
         
         #type
@@ -96,3 +96,77 @@ class Item(pygame.sprite.Sprite):
         
     def draw(self):
         self.level.screen.blit(self.image, (self.rect.x,self.rect.y))
+        
+####################end of item########################################
+
+
+class Pointer(pygame.sprite.Sprite):
+    def __init__(self, level, *groups):
+        super(Pointer, self).__init__(*groups)
+        #the game level
+        self.level = level
+        #base image
+        self.level.warehouse.set_Img(0,0)
+        self.level.warehouse.set_colorkey(True,(255,0,0))
+        self.image = self.level.warehouse.get_Img().convert()
+        
+        
+        #type
+        self.choices = []
+        
+        #location
+        #self.firstflag = True
+        self.scrnx = 0
+        self.scrny = 0
+        #self.mapx = 0
+        #self.mapy = 0
+        
+    #def spawn(self,x,y):
+    #    self.mapx = x
+    #    self.mapy = y
+       
+        
+    def position(self,x,y):
+        self.scrnx = x
+        self.scrny = y
+        self.rect = pygame.rect.Rect((self.scrnx*self.level.tilex, self.scrny*self.level.tiley), self.image.get_size())
+    
+    def set_type(self, itype):
+        self.flavor = self.flavor_saver[itype]
+        #unselected
+        if itype == 0:
+            xind = 0
+            yind = 0
+            
+        #selected
+        if itype == 1:
+            xind = 0
+            yind = 1
+       
+            
+        self.level.warehouse.set_Img(xind,yind)
+        self.level.warehouse.set_colorkey(True,(255,0,0))
+        self.image = self.level.warehouse.get_Img().convert()
+    
+
+
+    def set_Index(self, x, y):
+        self.scrnx = x
+        self.rect.x = x*self.level.tilex
+        self.scrny = y
+        self.rect.y = y*self.level.tiley
+        
+    def get_Index(self, axis):
+        if axis == 'X':
+            return self.scrnx
+        if axis == 'Y':
+            return self.scrny
+        return -1
+        
+    def draw(self):
+        self.level.screen.blit(self.image, (self.rect.x,self.rect.y))
+        
+        
+        
+        
+        
